1860f60a7b8f1cedecadf5aa5b5e2ea62bc4ab88
POWLAIR <114736502+POWLAIR@users.noreply.github.com>
2023-11-23 16:47:58 +0100

    Changement Footer

--- my-vue-app/src/components/MyFooter.vue
+++ my-vue-app/src/components/MyFooter.vue
@@ -1,25 +1,51 @@
 <template>
     <!-- Pied de page fixé en bas de la page -->
-    <footer class="footer mt-auto py-3" style="background-color:#f8f9fa ;">
-        <div class="d-flex justify-content-between align-items-center mx-auto py-4 flex-wrap" style="width: 80%">
-            <a href="/#" class="d-flex align-items-center p-0 text-dark">
-                <img alt="logo" src="../assets/logo.png" width="30px" />
-                <span class="ms-4 h5 font-weight-bold">Devwares</span>
-            </a>
-            <small>&copy; Devwares, 2020. All rights reserved.</small>
-            <div>
-                <button class="btn btn-dark btn-flat p-2 ">
-                    <img src="../assets/logo.png" width="20px" height="20px"/>
-                </button>
-                <button class="btn btn-dark btn-flat p-2 m-1">
-                    <img src="../assets/logo.png" width="20px" height="20px"/>
-                </button>
-                <button class="btn btn-dark btn-flat p-2 ">
-                    <img src="../assets/logo.png" width="20px" height="20px"/>
-                </button>
-            </div>
-        </div>
-    </footer>
+    <footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </template>
 
 
