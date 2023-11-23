68a54cfbebcf01925b6854585fd921a444a0b426
Sam-rst <samuel.ressiot@gmail.com>
2023-11-23 13:49:44 +0100

    Procédure README

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,4 +1,18 @@
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugins} from 'bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugins)
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
 
 createApp(App).mount('#app')
