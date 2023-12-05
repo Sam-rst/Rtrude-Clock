2d026f28e1dd557e2d81eea173612be318d76764
Sam-rst <samuel.ressiot@gmail.com>
2023-12-05 23:56:08 +0100

    Route login de prêt

--- my-express-app/routes.js
+++ my-express-app/routes.js
@@ -1,16 +1,34 @@
-const express = require('express');
+const User = require('./database/Entity/User');
 
-
-const setupRoutes = (app, db) => {
-    app.get('/pays', (req, res) => {
-        db.all('SELECT * FROM Pays', [], (err, rows) => {
-            if (err) {
-                res.status(500).send(err.message);
-            } else {
-                res.json(rows);
-            }
-        });
+const setupRoutes = (app) => {
+    app.post('/login', async (req, res) => {
+      try {
+        console.log(req)
+        const user = await User.getUserByUsername(req.username);
+        if (!user) {
+          console.log('User not found');
+          return res.json({ passwordMatch: false });
+        }
+  
+        console.log('User found : ', user.username);
+        const passwordMatch = await User.verifyPassword(req.password, user);
+  
+        // Mauvais mot de passe
+        if (!passwordMatch) {
+          console.log('Incorrect password');
+          return res.json({ passwordMatch: false });
+        }
+        
+        // Mot de passe bon
+        console.log('Password correct !');
+        return res.json({ passwordMatch: true });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ passwordMatch: false, error: error.message });
+      }
     });
-};
+  };
+  
+
 
 module.exports = setupRoutes;
