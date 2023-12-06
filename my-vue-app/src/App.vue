ceed347f9567928450e57c26a797da162605d095
POWLAIR <114736502+POWLAIR@users.noreply.github.com>
2023-12-06 05:25:53 +0100

    Correction router link - creation des pages tiers

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -5,8 +5,15 @@
     </header>
 
     <main>
-      <MyHome />
+      <router-link to="/">
+      </router-link>
+
+      <router-link to="/login">
+      </router-link>
+
+      <router-view />
     </main>
+
     <footer class="footer-section">
       <MyFooter />
     </footer>
@@ -17,14 +24,12 @@
 <script>
 
 import NavBar from "./components/NavBar.vue";
-import MyHome from "./components/MyHome.vue";
 import MyFooter from "./components/MyFooter.vue";
 
 export default {
   name: "App",
   components: {
     NavBar,
-    MyHome,
     MyFooter,
   },
 
