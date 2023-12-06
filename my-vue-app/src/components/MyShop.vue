616fc2823976504b8bb2bc6dc34b37a429155315
POWLAIR <114736502+POWLAIR@users.noreply.github.com>
2023-12-06 07:47:54 +0100

    commit shop

--- my-vue-app/src/components/MyShop.vue
+++ my-vue-app/src/components/MyShop.vue
@@ -24,12 +24,12 @@
                 <!-- Start Column 1 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-3.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-3.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Nordic Chair</h3>
                         <strong class="product-price">$50.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -38,12 +38,12 @@
                 <!-- Start Column 2 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-1.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-1.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Nordic Chair</h3>
                         <strong class="product-price">$50.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -52,12 +52,12 @@
                 <!-- Start Column 3 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-2.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-2.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Kruzo Aero Chair</h3>
                         <strong class="product-price">$78.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -66,12 +66,12 @@
                 <!-- Start Column 4 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-3.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-3.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Ergonomic Chair</h3>
                         <strong class="product-price">$43.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -81,12 +81,12 @@
                 <!-- Start Column 1 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-3.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-3.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Nordic Chair</h3>
                         <strong class="product-price">$50.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -95,12 +95,12 @@
                 <!-- Start Column 2 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-1.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-1.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Nordic Chair</h3>
                         <strong class="product-price">$50.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -109,12 +109,12 @@
                 <!-- Start Column 3 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-2.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-2.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Kruzo Aero Chair</h3>
                         <strong class="product-price">$78.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
@@ -123,12 +123,12 @@
                 <!-- Start Column 4 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5">
                     <a class="product-item" href="#">
-                        <img src="images/product-3.png" class="img-fluid product-thumbnail">
+                        <img src="../assets/images/product-3.png" class="img-fluid product-thumbnail">
                         <h3 class="product-title">Ergonomic Chair</h3>
                         <strong class="product-price">$43.00</strong>
 
                         <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
+                            <img src="../assets/images/cross.svg" class="img-fluid">
                         </span>
                     </a>
                 </div>
